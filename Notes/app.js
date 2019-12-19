03f118ea3b2b72ef85269964639860c568d71afa
Adrian Viquez <aviquez96@gmail.com>
2019-12-19 02:04:24 -0600

    Initial Commit

---
+++ Notes/app.js
@@ -0,0 +1,24 @@
+console.log("Starting app.js")
+
+const fs = require('fs');
+const _ = require('lodash');
+
+//Specifies everything that is exported from notes.js
+const notes = require('./notes.js');
+
+//Returns an array containing the command line arguments passed when the node process is launched
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log(process.argv);
+
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Reading note');
+} else if (command === 'remove') {
+    console.log('Removing note')
+} else {
+    console.log('Command not recognized');
+}
